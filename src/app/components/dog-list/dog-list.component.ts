5c9e106c92410623cb323624245c3e3c3e3dbca9
Shen Tang <adatang23@yahoo.com>
2025-06-16 18:38:20 +0000

    completed the Angular - Model Class Lab

--- src/app/components/dog-list/dog-list.component.ts
+++ src/app/components/dog-list/dog-list.component.ts
@@ -29,9 +29,12 @@ export class DogListComponent implements OnInit {
    * you should call the dogsService.addDog(newDog); method.
    */
   addDog(){
+    let newDog: Dog = {name: this.nameInput, breed: this.breedInput, age: this.ageInput};
+    this.dogsService.addDog(newDog);
   }
 
   ngOnInit(): void {
+    this.addDog();
   }
 
 }
